1d86025dad11193ef3ca38327404778b7f27845f
Helene Liobard <77423175+Helene-33@users.noreply.github.com>
2022-04-08 19:19:51 -0700

    update code 04/08/2022

--- 1_Intro_to_Node.js/index.js
+++ 1_Intro_to_Node.js/index.js
@@ -1,42 +1,27 @@
-const express = require('express'); 
-const app = express(); //create express app
-const port = 3000; //save port number
+const http = require('http'); //create variable http
+const PORT = 3000; //establish a port
 
-app.get('/', function(req, res) { //get a request to the root '/'
-    res.sendFile('index.html', {root: __dirname}); //server response by sending the index.html file to the client's browser
-});
-
-app.listen(port, () => { //server listens for any attempts from a client to connect at port 3000
-    console.log('Now listening on port ${port}');
-});
-
-
-
-
-
-
-
-/*
-const http = require("http");
-const fs = require("fs");
-http.createServer((req,res) => {
-    var path = req.url.toLowerCase();
-    switch(path) {
-        case '/':
-            fs.readFile("home.html", (err, data) => {
-             if (err) return console.error(err);
-                res.writeHead(200, {'Content-Type': 'text/html'});
-             res.end(data.toString());
-            });
-            break;
-        case '/about':
-            res.writeHead(200, {'Content-Type': 'text/plain'});
-            res.end('About page');
-            break;
-        default:
-            res.writeHead(404, {'Content-Type': 'text/plain'});
-            res.end('Not found');
-            break;
+const server = http.createServer( // create variable server
+    (req, res) => { // when request is made -> send response
+        const path = req.url.toLowerCase(); // path is url entered by user
+        switch(path){ // like a if statement
+            case '/': // if there is / after localhost:3000 then take user to home page
+                res.writeHead(200, {'Content-Type': 'text/plain'});
+                res.write('Hello there! Welcome to the home page.'); //what will be on the page
+                res.end();
+                break;
+            case '/about':
+                res.writeHead(200, {'Content-Type': 'text/plain'});
+                res.write('That is our about page.');
+                res.end();
+            default: // if anything else than / or /about is typed then display error page
+                res.writeHead(504, {'Content-Type': 'text/plain'});
+                res.end('404: Page not found.');
+                break; 
+        }
     }
-}).listen(process.env.PORT || 3000);
-*/
+)
+
+server.listen(PORT, () => { 
+    console.log('This server is running on port 3000') //display message on terminal when we run the server
+});
