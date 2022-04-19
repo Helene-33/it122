3e9aa8cfc4a3ff2da94bd43bb8d4554f60118ad8
Helene Liobard <77423175+Helene-33@users.noreply.github.com>
2022-04-18 17:44:55 -0700

    Add files via upload

--- test.js
+++ test.js
@@ -1,37 +1,38 @@
-import * as data from './data.js';
-import books from './data.js';
+const PORT = 3000; //establish a port
 
+//const http = require('http'); //create variable http
+import http from 'http'; // replace line 3 because of "type" : "module" in package.json
+
+import {getAll, getItem} from './data.js';
 
 import { parse } from "querystring";
-
-import http from 'http'; // replace line 4 because of "type" : "module" in package.json
-const PORT = 3000; //establish a port
 
 const server = http.createServer( // create variable server
     (req, res) => { // when request is made -> send response
         const path = req.url.toLowerCase(); // path is url entered by user
-        switch(path){ // like a if statement
+        
+        let url_parts = req.url.split("?"); // separate route from query string
+        
+        let query = parse(url_parts[1]); // convert query string to a JS object
+        
+        switch(url_parts[0]){ // like a if statement
             case '/': // if there is / after localhost:3000 then take user to home page
-                //let allBooks = data.getAll();
-                res.writeHead(200, {'Content-Type': 'text/html'});
+                res.writeHead(200, {'Content-Type': 'text/plain'});
                 res.write('Hello there!\r\nWelcome to the home page.\r\nHere is our books data:\r\n'); //what will be on the page
-                res.end(JSON.stringify(books, undefined, 5));
+                res.write(getAll());
+                res.end();
                 break;
             case '/about':
-                res.writeHead(200, {'Content-Type': 'text/html'});
+                res.writeHead(200, {'Content-Type': 'text/plain'});
                 res.write('That is our about page.');
                 res.end();
                 break;
-            case '/detail:':
-                let url = req.url.split("?"); // separate route from query string
-                let query = parse(url[1]); // convert query string to a JS object
-                const booksTitle = Text(request.params.title);
-                const getItem = books.find((book) => book.title === bookTitle);
+            case '/detail':
                 res.writeHead(200, {'Content-Type': 'text/plain'});
-                res.write();
-                res.end(data.getItem());
+                //res.write(getItem('1937'));
+                res.end('Detail for ' + query["year"]);
                 break;
-            default: // if anything else is typed then display error page
+            default: // if anything else than / or /about is typed then display error page
                 res.writeHead(504, {'Content-Type': 'text/plain'});
                 res.end('404: Page not found.');
                 break; 
@@ -42,5 +43,4 @@ const server = http.createServer( // create variable server
 server.listen(PORT, () => { 
     console.log('This server is running on port 3000') //display message on terminal when we run the server
 });
-
 
