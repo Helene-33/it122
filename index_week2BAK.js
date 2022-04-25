d61d6d6935808caa5a9a28ef1c5c877eb82b4d8c
Helene Liobard <77423175+Helene-33@users.noreply.github.com>
2022-04-24 18:36:39 -0700

    Fixing detail route

--- index_week2BAK.js
+++ index_week2BAK.js
@@ -5,10 +5,17 @@ import http from 'http'; // replace line 3 because of "type" : "module" in packa
 
 import {getAll, getItem} from './data.js';
 
+import { parse } from "querystring";
+
 const server = http.createServer( // create variable server
     (req, res) => { // when request is made -> send response
         const path = req.url.toLowerCase(); // path is url entered by user
-        switch(path){ // like a if statement
+        
+        let url_parts = req.url.split("?"); // separate route from query string
+        
+        let query = parse(url_parts[1]); // convert query string to a JS object
+        
+        switch(url_parts[0]){ // like a if statement
             case '/': // if there is / after localhost:3000 then take user to home page
                 res.writeHead(200, {'Content-Type': 'text/plain'});
                 res.write('Hello there!\r\nWelcome to the home page.\r\nHere is our books data:\r\n'); //what will be on the page
@@ -22,8 +29,8 @@ const server = http.createServer( // create variable server
                 break;
             case '/detail':
                 res.writeHead(200, {'Content-Type': 'text/plain'});
-                res.write(getItem('1937'));
-                res.end();
+                //res.write(getItem('1937'));
+                res.end('Detail for ' + query["title"]);
                 break;
             default: // if anything else than / or /about is typed then display error page
                 res.writeHead(504, {'Content-Type': 'text/plain'});
