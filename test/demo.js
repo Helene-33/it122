4441d4f3ed30b3d607fe97da47e8f3aa61855752
Helene Liobard <77423175+Helene-33@users.noreply.github.com>
2022-05-23 15:17:39 -0700

    Update demo.js after feedback

--- test/demo.js
+++ test/demo.js
@@ -1,20 +1,28 @@
 import { expect } from "chai";
 import * as book from "../data.js";
+import {getAll, getItem} from './data.js';
 
 //let result = {title : "Little Women", author : "Louisa May Alcott", year : 1868, genres : ["Novel", "Fiction", "Children's literature"]}
 
 
-const getItem = (item) => {
+/*const getItem = (item) => {
     return {title : "Little Women", author : "Louisa May Alcott", year : 1868, genres : ["Novel", "Fiction", "Children's literature"]}
-}
+}*/
 
+//test all book
+describe("test books data", () => {
+    it('test all the data', () => {
+        let result = getAll ();
+        expect(result).to.deep.include({books})
+    });
+})
 
 describe('test deep equality', () => {
     //get a book
     it('getItem returns correct the correct book', () => {
-        let result = getItem("Little Women");
+        let result = getItem(req.query.title);
         expect(result).to.deep.equal(
-            {title : "Little Women", author : "Louisa May Alcott", year : 1868, genres : ["Novel", "Fiction", "Children's literature"]}
+            {title : req.query.title, author : req.query.author, year : req.query.year, genres : req.query.genres}
         )   
     });
    
@@ -39,9 +47,9 @@ describe('test deep equality', () => {
 
       //delete a book
     it('deleteItem removed the book', () => {
-        let result = deleteItem("Little Women");
+        let result = deleteItem(req.query.title);
         expect(result).to.deep.equal(
-            {title : "Little Women", author : "Louisa May Alcott", year : 1868, genres : ["Novel", "Fiction", "Children's literature"]}
+            {title : req.query.title, author : req.query.author, year : req.query.year, genres : req.query.genres}
         )   
     });
    
